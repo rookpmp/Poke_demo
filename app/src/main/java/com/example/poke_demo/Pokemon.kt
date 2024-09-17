57179127fa4735843026d882a1cf4ce3c8f628d3
Beatriz Guerra <beatrizespadaguerra@gmail.com>
2024-09-17 17:57:40 +0100

    criar classe pokemon

---
+++ app/src/main/java/com/example/poke_demo/Pokemon.kt
@@ -0,0 +1,11 @@
+package com.example.poke_demo
+
+class Pokemon {
+    val nome: String = ""
+    val hp: Int = 0
+    val atk: Int = 0
+    val dfs: Int = 0
+    val spd: Int = 0
+    val spAtk: Int = 0
+
+}
