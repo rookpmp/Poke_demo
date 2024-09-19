384fd22917a844604adb5cf7a994ba1f73c3a0a8
Beatriz Guerra <beatrizespadaguerra@gmail.com>
2024-09-19 17:34:10 +0100

    add PokemonList e nav ok

--- app/src/main/java/com/example/poke_demo/MainActivity.kt
+++ app/src/main/java/com/example/poke_demo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,16 +12,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.poke_demo.ui.theme.Poke_demoTheme
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+// © 2024 [Beatriz Guerra, Pedro Ferreira, Pedro Pires, Tayara Cruz]
+
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: PokemonViewModel by viewModels {
+        PokemonViewModelFactory(PokemonRepository(NetworkModule.api))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var navController = rememberNavController()
 
+            NavHost(
+                navController = navController,
+                startDestination = "pokemon_list",
+                builder = {
+                    composable("pokemon_list") {
+                        PokemonListScreen(navController, viewModel)
+                    }
+                }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/poke_demo/PokemonList.kt
@@ -0,0 +1,89 @@
+package com.example.poke_demo
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import java.util.Locale
+
+// © 2024 [Beatriz Guerra, Pedro Ferreira, Pedro Pires, Tayara Cruz]
+
+@Composable
+fun PokemonListScreen(navController: NavController, viewModel: PokemonViewModel) {
+    val pokemonList = viewModel.pokemonList
+    val isLoading by remember { mutableStateOf(viewModel.isLoading) }
+
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(listState) {
+        snapshotFlow { listState.layoutInfo.visibleItemsInfo.lastOrNull()?.index }
+            .collect { lastVisibleItemIndex ->
+                if (lastVisibleItemIndex == pokemonList.size - 1 && !isLoading) {
+                    viewModel.loadPokemonList()
+                }
+            }
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.pokedex_logo),
+            contentDescription = "Logo do Pokedex",
+            modifier = Modifier.size(250.dp))
+
+        LazyColumn(state = listState) {
+            items(pokemonList) { pokemon ->
+                PokemonListItem(pokemon = pokemon, onClick = {
+                    val id = pokemon.url.split("/").filter { it.isNotEmpty() }.last().toInt()
+                    navController.navigate("pokemon_detail/$id")
+                })
+            }
+            if (isLoading) {
+                item {
+                    CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun PokemonListItem(pokemon: PokemonEntry, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { onClick() }
+    ) {
+        Text(
+            text = pokemon.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() },
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
